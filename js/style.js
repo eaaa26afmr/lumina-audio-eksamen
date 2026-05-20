be35b80e3bad94f5689f8239cd9a2e60aa77023e
eaaa26afmr <eaa26afmr@students.eaaa.dk>
2026-05-20 17:38:29 +0200

    Har sat kommentar rundt om mit indhold i JS

--- js/style.js
+++ js/style.js
@@ -1,15 +1,29 @@
 "use strict";
 
+// Finder HTML-elementet med klassen .slides (en container med billeder)//
 const slides = document.querySelector(".slides");
+
+// Finder alle HTML-elementerne med klassen .dot (farve cirklerne under slideren)//
 const dots = document.querySelectorAll(".dot");
+
+// Opretter variabelen index, som holder styr på det aktuelle billede i slideren. 
+// Starter ved 0, hvilket betyder det første billede.//
 let index = 0;
 
+//En funktion der opdaterer slideren// 
 function updateSlide() {
+
+
+    //Flytter slideren horizontalt ved at bruge transform: translateX() CSS-egenskaben//
     slides.style.transform = `translateX(${-index * 100}%)`;
+
+    //Fjerner klassen 'active' fra alle dots, og tilføjer den til den aktuelle dot baseret på index//
     dots.forEach(dot => dot.classList.remove("active"));
+
+    //Tilføjer klassen 'active' til den dot, der svarer til det aktuelle billede i slideren//
     dots[index].classList.add("active");
 }
-
+//Tilføjer et klik-event listener til hver dot, som opdaterer index og slideren, når en dot klikkes//
 dots.forEach((dot, i) => {
     dot.addEventListener("click", () => {
         index = i;
