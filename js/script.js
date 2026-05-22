b4ca01fdacf051b6673c8630ea0543a8fbebf0c5
eaaa26afmr <eaa26afmr@students.eaaa.dk>
2026-05-22 19:20:10 +0200

    Ændret baggrundsfarven

--- js/script.js
+++ js/script.js
@@ -9,6 +9,17 @@ const dots = document.querySelectorAll(".dot");
 // Finder HTML-elementet med id'et #color-name (elementet der viser farvenavnet)//
 const colorName = document.querySelector("#color-name");
 
+// Finder HTML-elementet med klassen .product-showcase (containeren for produktbillederne)//
+const showcase = document.querySelector(".product-showcase");
+
+// Opretter et array med CSS-gradienter, som bruges til at ændre baggrundsfarven på produkt-showcasen, når slideren opdateres//
+const colors = [
+  "linear-gradient(to bottom, #f4d7d7 0%, #ffffff %)",
+  "linear-gradient(to bottom, #f5f1e8 0%, #ffffff 100%)",
+  "linear-gradient(to bottom, #e6d9ea 0%, #ffffff 100%)",
+  "linear-gradient(to bottom, #dfe8d7 0%, #ffffff 100%)"
+];
+
 // Opretter variabelen index, som holder styr på det aktuelle billede i slideren. 
 // Starter ved 0, hvilket betyder det første billede.//
 let index = 0;
@@ -16,9 +27,10 @@ let index = 0;
 //En funktion der opdaterer slideren// 
 function updateSlide() {
 
-
     //Flytter slideren horizontalt ved at bruge transform: translateX() CSS-egenskaben//
     slides.style.transform = `translateX(${-index * 100}%)`;
+    //Opdaterer baggrundsfarven på produkt-showcasen baseret på det aktuelle index//
+    showcase.style.background = colors[index];
 
     //Fjerner klassen 'active' fra alle dots, og tilføjer den til den aktuelle dot baseret på index//
     dots.forEach(dot => dot.classList.remove("active"));
