502644ce5afc386466a8998bc10141211558f821
eaaa26afmr <eaa26afmr@students.eaaa.dk>
2026-05-24 15:05:42 +0200

    Done

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const showcase = document.querySelector(".product-showcase");
 
 // Opretter et array med CSS-gradienter, som bruges til at ændre baggrundsfarven på produkt-showcasen, når slideren opdateres//
 const colors = [
-  "linear-gradient(to bottom, #f4d7d7 0%, #ffffff %)",
+  "linear-gradient(to bottom, #f4d7d7 0%, #ffffff 100%)",
   "linear-gradient(to bottom, #f5f1e8 0%, #ffffff 100%)",
   "linear-gradient(to bottom, #e6d9ea 0%, #ffffff 100%)",
   "linear-gradient(to bottom, #dfe8d7 0%, #ffffff 100%)"
